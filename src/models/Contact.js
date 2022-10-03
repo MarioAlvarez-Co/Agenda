db7f40b8e89113447c1d9951edb9513a2bf4797b
Mario <lushoalvarez45@gmail.com>
2022-10-03 00:10:09 -0500

    creacion de los modelos

---
+++ src/models/Contact.js
@@ -0,0 +1,11 @@
+export class Contact{
+    name='';
+    last_name='';
+    email='';
+    online=false;
+    constructor(name,last_name,email){
+        this.name=name;
+        this.last_name=last_name;
+        this.email=email;
+    }
+}
